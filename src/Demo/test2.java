bdff09998bafd76f2b8a8ad221e6ee42b62b1b74
testing-githubdemo <patilpoonam726@gmail.com>
2023-01-07 21:12:02 +0530

    This is my first commit

---
+++ src/Demo/test2.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int a=20;
+  int b=30;
+  System.out.println(a+b);
+	}
+
+}
