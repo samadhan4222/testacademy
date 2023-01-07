19bac45aa92a752174a0a318132a733eb4740ab6
My PC <My PC@DESKTOP-M5VPEBL>
2023-01-07 21:29:27 +0530

    Changes on test1 ,test3

--- src/Demo/test3.java
+++ src/Demo/test3.java
@@ -1,11 +1,10 @@
 package Demo;
 
-public class Test1 {
+public class test3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  System.out.println("hello");
-  System.out.println("Python");
+System.out.println("Hello java");
 	}
 
 }
